ecc64483cddb6f370b88925c23775ec20719a505
Laôni A. Moreira <laonimoreira.capanema@gmail.com>
2025-03-10 14:13:45 -0300

    feat: run backend when ready frontend

--- src/main/main.js
+++ src/main/main.js
@@ -1,11 +1,18 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const { exec } = require("child_process");
 require('./ipc/concessionariasIpc'); 
 require('./ipc/serviceIpc'); 
 
 let mainWindow;
 
 app.whenReady().then(() => {
+    const backendPath = '../../../AutoAMBAR/backend/dist/main.exe';
+
+    const backendProcess = exec(backendPath, (error) => {
+        if (error) console.error("Erro ao iniciar o backend:", error);
+    });
+
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
